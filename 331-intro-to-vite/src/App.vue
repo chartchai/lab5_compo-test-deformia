14925462ed87a67141f6cd57ad4693fa587d6593
DeformiaR <113496368+DeformiaR@users.noreply.github.com>
2024-08-09 02:46:39 +0700

    3.2

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -29,13 +29,6 @@ const { message } = storeToRefs(store)
 </template>
 
 <style scoped>
-#layout {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 nav {
   padding: 30px;
